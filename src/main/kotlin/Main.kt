3b8c05a838229c18e94e70423c62afdd29372342
mkulak <kleshney@gmail.com>
2017-09-08 21:38:52 +0200

    add server with endpoints + logging

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,25 @@
+import io.vertx.core.Vertx
+import io.vertx.ext.web.Router
+import org.slf4j.LoggerFactory
 
 fun main(args: Array<String>) {
-    println("Hello world")
+    val logger = LoggerFactory.getLogger("Main")
+
+    val port = 8080
+    val vertx = Vertx.vertx()
+    val router = Router.router(vertx)
+    router.post("/transactions").handler { ctx ->
+        ctx.response().setStatusCode(201).end()
+    }
+    router.get("/statistics").handler { ctx ->
+        ctx.response().setStatusCode(200).end("hello")
+    }
+    vertx.createHttpServer().requestHandler(router::accept).listen(port) {
+        if (it.succeeded()) {
+            logger.info("Server started on $port")
+        } else {
+            logger.error("Fail to start server: ${it.cause()}")
+            vertx.close()
+        }
+    }
 }
