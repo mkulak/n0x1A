becc97c76a4cc5228b08e4896d23cd519dadf42d
mkulak <kleshney@gmail.com>
2017-09-09 00:48:16 +0200

    fix http api tests

--- src/test/kotlin/com/text/n0x10/HttpApiTest.kt
+++ src/test/kotlin/com/text/n0x10/HttpApiTest.kt
@@ -16,6 +16,7 @@ import java.net.ServerSocket
 @RunWith(VertxUnitRunner::class)
 class HttpApiTest {
     val vertx = Vertx.vertx()
+    val client = WebClient.create(vertx)
     val port = getRandomFreePort()
     val statsManager = TestStatsManager(Stats(100.0, 11.0, -1.0, 99.0, 4))
 
@@ -33,26 +34,26 @@ class HttpApiTest {
 
     @Test
     fun `statistics should respond with result from StatsManager`(context: TestContext) {
-        val client = WebClient.create(vertx)
+        val async = context.async()
         client.get(port, "localhost", "/statistics").send {
             if (it.succeeded()) {
                 val response = it.result()
                 context.assertEquals(200, response.statusCode())
                 val received = Json.mapper.readTree(response.body().toString())
                 val expected = statsManager.response
-                context.assertEquals(expected.sum, received["sum"])
-                context.assertEquals(expected.avg, received["avg"])
-                context.assertEquals(expected.min, received["min"])
-                context.assertEquals(expected.max, received["max"])
-                context.assertEquals(expected.count, received["count"])
-                context.async().complete()
+                context.assertEquals(expected.sum, received["sum"].asDouble())
+                context.assertEquals(expected.avg, received["avg"].asDouble())
+                context.assertEquals(expected.min, received["min"].asDouble())
+                context.assertEquals(expected.max, received["max"].asDouble())
+                context.assertEquals(expected.count, received["count"].asInt())
+                async.complete()
             } else context.fail(it.cause())
         }
     }
 
     @Test
     fun `transactions should respond with 201`(context: TestContext) {
-        val client = WebClient.create(vertx)
+        val async = context.async()
         val amount = 12.0
         val timestamp = 100500L
         val body = mapOf("amount" to amount, "timestamp" to timestamp)
@@ -61,7 +62,7 @@ class HttpApiTest {
                 val response = it.result()
                 context.assertEquals(201, response.statusCode())
                 context.assertEquals(Transaction(amount, timestamp), statsManager.received.get())
-                context.async().complete()
+                async.complete()
             } else context.fail(it.cause())
         }
     }
